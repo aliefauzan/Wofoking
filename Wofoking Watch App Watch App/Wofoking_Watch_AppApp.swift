c9652b3d1109487222e8b18af53635212bb305e0
aliefauzan <afindo.mi01@gmail.com>
2026-06-25 23:52:12 +0800

    init

---
+++ Wofoking Watch App Watch App/Wofoking_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wofoking_Watch_AppApp.swift
+//  Wofoking Watch App Watch App
+//
+//  Created by Andi Muhammad Alief Fauzan on 25/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Wofoking_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
