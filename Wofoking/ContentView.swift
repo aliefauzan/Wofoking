8fbd87bcb7621064e60c2a192a6093b3d08e17fd
aliefauzan <afindo.mi01@gmail.com>
2026-06-25 12:09:11 +0800

    Initial Commit

---
+++ Wofoking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wofoking
+//
+//  Created by Andi Muhammad Alief Fauzan on 25/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
